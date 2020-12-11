0dad0661061bd0fb306313eea7bf2251337da8bb
Dipesh Makwana <dipesh_makwana@hotmail.co.uk>
2020-12-11 13:25:23 +0000

    Enum Created For Facing Direction

---
+++ ToyRobot/FacingDirection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToyRobot
+{
+    public enum FacingDirection
+    {
+        NORTH,
+        SOUTH,
+        WEST,
+        EAST
+    }
+}
